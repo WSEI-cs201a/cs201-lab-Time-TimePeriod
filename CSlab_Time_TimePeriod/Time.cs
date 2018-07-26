b783b2cc5a822b7daa678427d481ac880482bc8b
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2018-07-26 23:41:56 +0200

    wersja 1

--- CSlab_Time_TimePeriod/Time.cs
+++ CSlab_Time_TimePeriod/Time.cs
@@ -1,14 +1,16 @@
 using System;
 
-public struct Time 
+namespace CSLabs.TimeLib
 {
+    public struct Time
+    {
         public byte Hours { get; private set; }
-        public byte Minutes{ get; private set; }
-        public byte Seconds{ get; private set; }
+        public byte Minutes { get; private set; }
+        public byte Seconds { get; private set; }
 
 
         public Time(byte hours, byte minutes, byte seconds)
-        {            
+        {
             if (hours < 0 || hours > 23) throw new ArgumentOutOfRangeException("Liczba godzin musi byc z przedzialu od 0 do 23");
             if (minutes < 0 || minutes > 59) throw new ArgumentOutOfRangeException("Liczba minut musi byc z przedzialu od 0 do 59");
             if (seconds < 0 || seconds > 59) throw new ArgumentOutOfRangeException("Liczba sekund musi byc z przedzialu od 0 do 59");
@@ -16,5 +18,7 @@ public struct Time
             Minutes = minutes;
             Seconds = seconds;
         }
+
+    }
 
 }
--- CSlab_Time_TimePeriod/TimePeriod.cs
+++ CSlab_Time_TimePeriod/TimePeriod.cs
@@ -1,15 +1,15 @@
 using System;
 
-namespace TmieLib
+namespace CSLabs.TimeLib
 {
-public struct TimePeriod 
-{
-    private readonly long seconds;
+    public struct TimePeriod
+    {
+        private readonly long seconds;
         public TimePeriod(byte hh, byte mm, byte ss)
-        {            
+        {
             throw new NotImplementedException();
         }
 
-}
+    }
 
 }
