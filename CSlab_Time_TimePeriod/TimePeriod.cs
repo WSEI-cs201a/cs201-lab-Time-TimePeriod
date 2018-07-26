c04bbda34f8cf51194906ba079335b04bef92e80
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2018-07-26 23:40:12 +0200

    wersja 1

---
+++ CSlab_Time_TimePeriod/TimePeriod.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TmieLib
+{
+public struct TimePeriod 
+{
+    private readonly long seconds;
+        public TimePeriod(byte hh, byte mm, byte ss)
+        {            
+            throw new NotImplementedException();
+        }
+
+}
+
+}
