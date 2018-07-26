e7626de17886e94ff55b6b5269e403d27d14e32b
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2018-07-26 22:39:03 +0200

    Initial commit

---
+++ CSlab_Time_TimePeriod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSlab_Time_TimePeriod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
